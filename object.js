11d26107fba8a664076e480a840dc28a89b81b00
Romana Rashid <romanacse98@gmail.com>
2022-02-02 22:53:10 +0600

    object added

---
+++ object.js
@@ -0,0 +1,15 @@
+var student1 = {
+    id: 7,
+    name: 'Romana Rashid',
+    marks: 81,
+    phone: 093734
+}
+console.log(student1.name);
+
+var mobile = {
+    color: 'black',
+    price: 2000,
+    Storage: '16gb',
+    screensize: 5
+};
+console.log(mobile.price);
